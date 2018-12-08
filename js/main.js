58825ccb42ed96d972b803e023e0ca69fb51b2b7
Roger Rodriguez <evolpirate@gmail.com>
2018-12-07 22:10:32 -0500

    responsive header for height of display

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("top-button").style.display = "block";
-    } else {
-        document.getElementById("top-button").style.display = "none";
+    // fires when the window scrolls
+    window.onscroll = function() { scrollFunction() };
+
+    // hiding and displaying top button
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("top-button").style.display = "block";
+        } else {
+            document.getElementById("top-button").style.display = "none";
+        }
     }
-}
 
-// When the user clicks on the button go back tot h
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+    // When the user clicks on the button go back to top id=top-button
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+    // setting the height of the header to be the heihgt of the display
+    function setHeight() {
+      windowHeight = window.innerHeight.toString() + 'px';
+      document.getElementById('full-header').style.height = windowHeight;
+    };
+    
+    // set height of header
+    setHeight();
+
+    // whenever the window is resize run this
+    window.onresize = setHeight;
